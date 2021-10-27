48b0c2879823585bb35dc39b803ba6a4f24bb8f9
jsoyun <syusmm233@gmail.com>
2021-10-27 17:39:49 +0900

    객체확인(fruitname)

--- src/component/Page.js
+++ src/component/Page.js
@@ -2,23 +2,24 @@ import React from "react";
 
 // url  파라미터
 const data = {
+  //apple->fruitname
   apple: {
-    name: "사과",
+    name: "사과는 맛있어",
     description: "맛난과일",
   },
   grape: {
-    name: "포도",
+    name: "포도는 맛있어",
     description: "알럽 샤인머스켓",
   },
 };
 const Page = ({ match }) => {
+  //  /page/:fruitname
   const { fruitname } = match.params;
   const info = data[fruitname];
   return (
     <div>
-      <h1>
-        {fruitname}({info.name})
-      </h1>
+      <h1>{fruitname}</h1>
+      <h2>({info.name})</h2>
       <h4>{info.description}</h4>
     </div>
   );
